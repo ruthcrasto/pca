e6c29dfe484e7f95fb3bfe1c3b563ea75e401679
ruth.crasto <ruth.crasto@mail.utoronto.ca>
2020-04-20 20:46:26 -0400

    finalized visualization for prob-pca

--- prob_pca.py
+++ prob_pca.py
@@ -12,14 +12,17 @@ class ProbabilisticPCA:
     Arguments:
         X: a (N x d) or (N x w x h) data matrix (NumPy array)
     """
-    self.data = reshape_data(X)
+    X = X[:1000,:]
+    self.original_dims = X.shape
+    self.data = self.reshape_data(X)
+    d = self.data.shape[1]
     # hyperparameters
-    self.num_iters = 5
+    self.num_iters = 10
     self.embed_dim = k
     # initialize parameters
-    self.sigma_squared = ... # scalar
-    self.W = ... # d x k
-    self.mu = ... # d x 1
+    self.sigma = np.random.randn()
+    self.W = np.random.randn(d,k)
+    self.train()
 
   def reshape_data(self, X):
     """ Reshape 3D image data to 2D data.
@@ -35,23 +38,54 @@ class ProbabilisticPCA:
     return X
 
   def train(self):
-    """ Determine principal components using SVD.
+    """ Determine principal components using EM.
     """
-    X_centered = self.data - np.mean(self.data, axis=0)
-    N = X_centered.shape[0]
+    X_bar = self.data - np.mean(self.data, axis=0)
+    N,d = X_bar.shape
     for _ in range(self.num_iters):
         M = self.W.T @ self.W + self.sigma * np.eye(self.embed_dim)
         M_inv = np.linalg.inv(M)
 
         # E step
-        Z = M_inv @ W.T @ X_centered # N x k
-        temp = np.dot(Z.expand_dims(2), Z.expand_dims(1))
-        #temp = np.zeros(N, self.embed_dim, self.embed_dim)
-        #for i in range(N):
-            #temp[i] = Z[i].reshape(-1,1) @ Z[i].reshape(1,-1)
-        Z_ = self.sigma * M_inv +  temp # N x k x k
-
+        Z = X_bar @ self.W @ M_inv.T  # N x k
+        temp = np.expand_dims(Z,2) @ np.expand_dims(Z,1) # N x k x k
+        Z_ = self.sigma * M_inv + temp # N x k x k
+      
         # M step
-        self.W = np.sum(np.dot(X_centered.expand_dims(2), Z.expand_dims(1)), axis=0)
-        
+        self.W = np.sum(np.expand_dims(X_bar,2) @ np.expand_dims(Z,1), axis=0)
+        self.W = self.W @ np.linalg.inv(np.sum(Z_, axis=0))
+        self.sigma = np.sum(np.linalg.norm(X_bar,axis=1)**2)
+        self.sigma -= np.sum(np.diagonal(2 * Z @ self.W.T @ X_bar.T))
+        self.sigma += np.sum(np.trace((Z_ @ self.W.T @ self.W).T))
+        self.sigma /= N * d
 
+  def get_principal_components(self):
+    if len(self.original_dims) == 3:
+      return self.W.T.reshape(-1, self.original_dims[1], self.original_dims[2])
+    return self.W.T
+
+  def project_all(self, X):
+    projector = self.W
+    return X @ self.W
+
+  def visualize(self, X, labels=None):
+    """ Visualize input 2D embeddings.
+
+    Arguments:
+      X: a (N' x d) data matrix (NumPy array)
+      labels: a (N' x 1) matrix of numeric labels (NumPy array) - optional
+    """
+    X = self.reshape_data(X)
+    projections = self.project_all(X)
+    x = projections[:,0]
+    y = projections[:,1]
+    colors = list(mcolors.get_named_colors_mapping().keys())[-19:-9]
+    
+    if labels is not None:
+      for i in range(len(labels)):
+        plt.scatter(x[i],y[i],color=colors[int(labels[i])])
+        plt.annotate(labels[i], (x[i],y[i]))
+    else:
+      plt.scatter(x,y)
+    plt.show()
+    
--- visualize.py
+++ visualize.py
@@ -2,16 +2,18 @@ import argparse
 from pca import PCA
 from prob_pca import ProbabilisticPCA
 import mnist
+import numpy as np
 
 
 if __name__ == "__main__":
-    methods = {"pca": PCA}
+    # TODO: refactor methods into a superclass
+    
+    methods = {"pca": PCA, "prob_pca": ProbabilisticPCA}
     train_images, train_labels, test_images, test_labels = mnist.load()
     
     parser = argparse.ArgumentParser()
     parser.add_argument("--method", help="The method for dimensionality reduction", default="pca")    
     args = parser.parse_args()
-
     dim_reducer = methods[args.method](train_images)
     dim_reducer.visualize(test_images[:80], test_labels[0:80])
     mnist.show(dim_reducer.get_principal_components())
